cf1b5fee617d3c291e34362a2058d1053960aa91
Bernadette Jacobs <120324462+Keyter95@users.noreply.github.com>
2025-06-22 19:32:34 +0200

    Router changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./css/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/test-demo">
+    <HashRouter basename="/test-demo">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
